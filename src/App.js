f785a14c1d782e24134aa7e28f3f27e1ec1a54f2
mb <mubeenullhaq@gmail.com>
2022-03-25 12:25:27 +0500

    cryptohunter first code push

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./Components/Header";
+import HomePage from "./Pages/HomePage";
+import CoinPage from "./Pages/CoinPage";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles(() => ({
+  App: {
+    background: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+  },
+}));
+function App() {
+  const classes = useStyles();
+  return (
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/coins/:id" element={<CoinPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,66 @@
+import {
+  AppBar,
+  Container,
+  MenuItem,
+  Select,
+  Toolbar,
+  Typography,
+  createTheme,
+  ThemeProvider,
+} from "@material-ui/core";
+import React from "react";
+import { makeStyles } from "@material-ui/core";
+import { CryptoState } from "../CryptoContext";
+
+const useStyles = makeStyles(() => ({
+  title: {
+    flex: 1,
+    color: "gold",
+    fontFamily: "Montserrat",
+    fontWeight: "bold",
+    cursor: "pointer",
+  },
+}));
+
+const Header = () => {
+  const classes = useStyles();
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+
+  const { currency, setCurrency } = CryptoState;
+  console.log(currency);
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            <Typography className={classes.title} variant="h6">
+              <a href="/">Crypto Hunter</a>
+            </Typography>
+            <Select
+              variant="outlined"
+              style={{
+                width: 100,
+                height: 40,
+                marginRight: 15,
+              }}
+              value={currency}
+              onChange={(e) => setCurrency(e.target.value)}
+            >
+              <MenuItem vlaue={"PKR"}>PKR</MenuItem>
+              <MenuItem value={"USD"}>USD</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
+};
+
+export default Header;
